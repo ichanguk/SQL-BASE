e4c9b9bee5a8d2164be67cf7839b382fb96fd631
changukLee <ckddnr5527@gmail.com>
2024-06-02 23:51:19 +0900

    feat: update practice1
    mission2(mynewpokemon 테이블 생성)

--- practice/practice1.sql
+++ practice/practice1.sql
@@ -1,3 +1,4 @@
+# mission1
 CREATE DATABASE pokemon;
 
 use pokemon;
@@ -9,4 +10,17 @@ create table mypokemon(
 
 insert into mypokemon (number, name, type) values (10, "caterpie", "bug"),
 	(25, "pikachu", "electric"), (133, "eevee", "normal");
+
+select * from mypokemon;    
+
+# mission2
+create table mynewpokemon(
+	number INT,
+    name VARCHAR(20),
+    type VARCHAR(10)
+);
+
+insert into mynewpokemon (number, name, type) values (77, "포니타", "불꽃"),
+	(132, "메타몽", "노말"), (151, "뮤", "에스퍼");
     
+select * from mynewpokemon
